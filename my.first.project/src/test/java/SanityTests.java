071b5a1a0c241207cd6c863da8a6218a9aea28bb
abel <abel@localhost>
2022-11-02 19:48:50 -0500

    create first maven project

---
+++ my.first.project/src/test/java/SanityTests.java
@@ -0,0 +1,14 @@
+import org.testng.annotations.Test;
+
+public class SanityTests {
+  @Test
+  public void canRunTestNGTest() {
+	  //Prerequisite
+  }
+  
+  @Test
+  public void canUseVar() {
+	  String myString = "Abel";
+	  var myVarString = "Abel";
+  }
+}
